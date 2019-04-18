030c3c4c28100f1f0c41d5aa795b8d7fd2128e64
yuanshen <yshen47@illinois.edu>
2019-04-18 16:58:43 -0500

    added coordinator struct

---
+++ coordinator/coordinator.go
@@ -0,0 +1,32 @@
+package coordinator
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"context"
+)
+
+// CoordinatorInterfaceServer is the server API for CoordinatorInterface service.
+type CoordinatorInterfaceServer interface {
+	OpenTransaction(context.Context, *Empty) (*Transaction, error)
+	CloseTransaction(context.Context, *Transaction) (*Feedback, error)
+	CommitTransaction(context.Context, *Empty) (*Feedback, error)
+	AbortTransaction(context.Context, *Empty) (*Feedback, error)
+}
+
+
+type Coordinator struct {
+}
+
+func (*Coordinator) OpenTransaction(ctx context.Context, req *Empty) (*Transaction, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method OpenTransaction not implemented")
+}
+func (*Coordinator) CloseTransaction(ctx context.Context, req *Transaction) (*Feedback, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CloseTransaction not implemented")
+}
+func (*Coordinator) CommitTransaction(ctx context.Context, req *Empty) (*Feedback, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CommitTransaction not implemented")
+}
+func (*Coordinator) AbortTransaction(ctx context.Context, req *Empty) (*Feedback, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AbortTransaction not implemented")
+}
