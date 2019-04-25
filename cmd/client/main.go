112b7780ba0ca5e56aa0ed169b68675141a2cb84
yuanshen <yshen47@illinois.edu>
2019-04-24 22:08:54 -0500

    added insecure config for dialing

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -9,21 +9,19 @@ import (
 )
 
 func main() {
+
 	serverPorts := [5]string {"5600", "5700", "5800", "5900", "6000"}
 	coordPort := "6100"
-
-
-	 // determine whether to use TLS
 
 	var serverConn [] server.NodeClient = make([] server.NodeClient, 5)
 	for i := 0; i<5; i++ {
 		ipaddr := utils.Concatenate("127.0.0.1",":",serverPorts[i])
-		conn, err := grpc.Dial(ipaddr)
+		conn, err := grpc.Dial(ipaddr, grpc.WithInsecure())
 		utils.CheckError(err)
 		serverConn[i] = server.NewNodeClient(conn)
 	}
 	coordAddr := utils.Concatenate("127.0.0.1",":",coordPort)
-	conn, error := grpc.Dial(coordAddr)
+	conn, error := grpc.Dial(coordAddr, grpc.WithInsecure())
 	coordConn := server.NewCoordinatorClient(conn)
 	utils.CheckError(error)
 	transactionID, err := coordConn.OpenTransaction(context.Background(),nil)
